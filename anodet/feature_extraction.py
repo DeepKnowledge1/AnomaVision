3ef9e683af9ade7b7b1c98b073ff340064f08121
DeepKnowledge1 <deepp.knowledge@gmail.com>
2025-08-22 13:20:06 +0200

    fxing
    RuntimeError: Input type (torch.FloatTensor) and weight type (torch.cuda.FloatTensor) should be the same or input should be a MKLDNN tensor and weight is a dense tensor

--- anodet/feature_extraction.py
+++ anodet/feature_extraction.py
@@ -97,11 +97,11 @@ class ResnetEmbeddingsExtractor(torch.nn.Module):
             embedding_vectors = embedding_vectors.reshape(batch_size, length, width*height)
             embedding_vectors = embedding_vectors.permute(0, 2, 1)
             
-            embedding_vectors = (
-                embedding_vectors.half()
-                if embedding_vectors.device.type != "cpu"
-                else embedding_vectors
-            )
+            # embedding_vectors = (
+            #     embedding_vectors.half()
+            #     if embedding_vectors.device.type != "cpu"
+            #     else embedding_vectors
+            # )
             
             return embedding_vectors,width, height
 
