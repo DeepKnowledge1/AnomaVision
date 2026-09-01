a0d137e628c2b12c7db6f2ac893ca27a32f66404
Deep Knowledge <66887716+DeepKnowledge1@users.noreply.github.com>
2026-09-01 08:34:09 +0200

    feat(actions): add industrial action package

---
+++ anomavision/actions/__init__.py
@@ -0,0 +1,7 @@
+"""Industrial output actions for AnomaVision."""
+
+from anomavision.actions.ActionBase import ActionBase
+from anomavision.actions.ActionFactory import ActionFactory
+from anomavision.actions.MQTTAction import MQTTAction
+
+__all__ = ["ActionBase", "ActionFactory", "MQTTAction"]
