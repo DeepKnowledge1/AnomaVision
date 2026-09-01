5082d5016ee786135b7428b37ef87c6603ecbae0
Deep Knowledge <66887716+DeepKnowledge1@users.noreply.github.com>
2026-09-01 11:36:47 +0200

    Export evidence action

--- anomavision/actions/__init__.py
+++ anomavision/actions/__init__.py
@@ -3,6 +3,13 @@
 from anomavision.actions.ActionBase import ActionBase
 from anomavision.actions.ActionDispatcher import ActionDispatcher
 from anomavision.actions.ActionFactory import ActionFactory
+from anomavision.actions.EvidenceAction import EvidenceAction
 from anomavision.actions.MQTTAction import MQTTAction
 
-__all__ = ["ActionBase", "ActionDispatcher", "ActionFactory", "MQTTAction"]
+__all__ = [
+    "ActionBase",
+    "ActionDispatcher",
+    "ActionFactory",
+    "EvidenceAction",
+    "MQTTAction",
+]
