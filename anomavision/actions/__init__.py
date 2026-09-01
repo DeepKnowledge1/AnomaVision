355c40b797cbf3d0e89257a1a7860d588b4ca6d9
Deep Knowledge <66887716+DeepKnowledge1@users.noreply.github.com>
2026-09-01 08:54:20 +0200

    feat: expose ActionDispatcher

--- anomavision/actions/__init__.py
+++ anomavision/actions/__init__.py
@@ -1,7 +1,8 @@
 """Industrial output actions for AnomaVision."""
 
 from anomavision.actions.ActionBase import ActionBase
+from anomavision.actions.ActionDispatcher import ActionDispatcher
 from anomavision.actions.ActionFactory import ActionFactory
 from anomavision.actions.MQTTAction import MQTTAction
 
-__all__ = ["ActionBase", "ActionFactory", "MQTTAction"]
+__all__ = ["ActionBase", "ActionDispatcher", "ActionFactory", "MQTTAction"]
