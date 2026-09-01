9fa4461d310be782e5979b8a01c99ccfa00b0694
Deep Knowledge <66887716+DeepKnowledge1@users.noreply.github.com>
2026-09-01 11:38:29 +0200

    feat(actions): register OPC UA action

--- anomavision/actions/ActionFactory.py
+++ anomavision/actions/ActionFactory.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, Iterable, List
 from anomavision.actions.ActionBase import ActionBase
 from anomavision.actions.EvidenceAction import EvidenceAction
 from anomavision.actions.MQTTAction import MQTTAction
+from anomavision.actions.OPCUAAction import OPCUAAction
 
 
 class ActionFactory:
@@ -15,6 +16,7 @@ class ActionFactory:
     _ACTIONS = {
         "mqtt": MQTTAction,
         "evidence": EvidenceAction,
+        "opcua": OPCUAAction,
     }
 
     @classmethod
