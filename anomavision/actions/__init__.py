c42b175fa681ffa156203b951da95145c9693f2c
Deep Knowledge <66887716+DeepKnowledge1@users.noreply.github.com>
2026-09-01 11:38:35 +0200

    feat(actions): export OPC UA action

--- anomavision/actions/__init__.py
+++ anomavision/actions/__init__.py
@@ -5,6 +5,7 @@ from anomavision.actions.ActionDispatcher import ActionDispatcher
 from anomavision.actions.ActionFactory import ActionFactory
 from anomavision.actions.EvidenceAction import EvidenceAction
 from anomavision.actions.MQTTAction import MQTTAction
+from anomavision.actions.OPCUAAction import OPCUAAction
 
 __all__ = [
     "ActionBase",
@@ -12,4 +13,5 @@ __all__ = [
     "ActionFactory",
     "EvidenceAction",
     "MQTTAction",
+    "OPCUAAction",
 ]
