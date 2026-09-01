e9a449b8c6518a4b31605b962020bb920ec8b43f
Deep Knowledge <66887716+DeepKnowledge1@users.noreply.github.com>
2026-09-01 11:35:25 +0200

    feat: expose inspection pipeline

--- anomavision/inspection/__init__.py
+++ anomavision/inspection/__init__.py
@@ -1,6 +1,7 @@
 """Production inspection primitives for AnomaVision."""
 
 from anomavision.inspection.DecisionEngine import DecisionEngine
+from anomavision.inspection.InspectionPipeline import InspectionPipeline
 from anomavision.inspection.InspectionResult import InspectionResult
 
-__all__ = ["DecisionEngine", "InspectionResult"]
+__all__ = ["DecisionEngine", "InspectionPipeline", "InspectionResult"]
